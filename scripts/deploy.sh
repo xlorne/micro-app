992b64ffce9ea1b671bac03b34cc796731053bd8
lorne <1991wangliang@gmail.com>
2024-09-10 22:57:07 +0800

    add webpack

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-scp -o ConnectTimeout=30 -P 8022  -r * root@hr-dev:/opt/test/
-ssh -p 8022 root@hr-dev "cd /opt/test/ && sed -i 's/\r//g' *.sh && sh install.sh"
+scp -o ConnectTimeout=30 -P 8022  -r * root@192.168.3.200:/opt/test/
+ssh -p 8022 root@192.168.3.200 "cd /opt/test/ && sed -i 's/\r//g' *.sh && sh install.sh"
