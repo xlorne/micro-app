1709198cb81dc37b965f2d6deb6e773ec2552066
lorne <1991wangliang@gmail.com>
2024-09-11 23:06:51 +0800

    update labels

--- mirco-home-dynamic/src/pages/NotFount.tsx
+++ mirco-home-dynamic/src/pages/NotFount.tsx
@@ -2,7 +2,18 @@ import React from "react";
 
 const NotFount = () => {
     return (
-        <>404</>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                flexDirection: 'column',
+                gap: '50px',
+            }}
+        >
+            <h1>Error 404 ,Page Not Found</h1>
+        </div>
     )
 }
 
